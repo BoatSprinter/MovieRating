97c8dfb2beb04a5d24f8a5773d2a92af3723f13c
morri <morrisan2001@gmail.com>
2025-02-12 05:04:08 +0100

    nav code to bulky so just stiped it down, but still need to ux it

--- frontend/src/shared/NavMenu.tsx
+++ frontend/src/shared/NavMenu.tsx
@@ -1,64 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NavMenu: React.FC = () => {
-  const [expanded, setExpanded] = useState(false);
-
   return (
-    <Navbar 
-      bg="dark" 
-      variant="dark" 
-      expand="lg" 
-      expanded={expanded}
-      onToggle={setExpanded}
-      className="mb-4 shadow"
-      style={{ width: '100%' }}
-    >
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand 
-          as={Link} 
-          to="/" 
-          onClick={() => setExpanded(false)}
-          style={{ color: 'white' }}
-        >
-          Movie Ranking
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
+        <Navbar.Brand as={Link} to="/">Movie Rating</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link 
-              as={Link} 
-              to="/" 
-              onClick={() => setExpanded(false)}
-              style={{ color: 'rgba(255,255,255,0.8)' }}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link 
-              as={Link} 
-              to="/movies" 
-              onClick={() => setExpanded(false)}
-              style={{ color: 'rgba(255,255,255,0.8)' }}
-            >
-              Movies
-            </Nav.Link>
-            <Nav.Link 
-              as={Link} 
-              to="/movies/create" 
-              onClick={() => setExpanded(false)}
-              style={{ color: 'rgba(255,255,255,0.8)' }}
-            >
-              Add Movie
-            </Nav.Link>
-            <Nav.Link 
-              as={Link} 
-              to="/about" 
-              onClick={() => setExpanded(false)}
-              style={{ color: 'rgba(255,255,255,0.8)' }}
-            >
-              About
-            </Nav.Link>
+            <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
+            <Nav.Link as={Link} to="/movies/create">Add Movie</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
