d0b0bd7d70f4ddb8c2c6f72214ed8d4f759daea4
morri <morrisan2001@gmail.com>
2025-02-12 05:08:57 +0100

    same for yesterday but had renamed to tsx
    check in index

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import NavMenu from './shared/NavMenu.tsx';
+import HomePage from './home/HomePage.tsx';
+import MovieListPage from './pages/MovieListPage.tsx';
+import MovieCreatePage from './pages/MovieCreatePage.tsx';
+import MovieDetails from './pages/MovieDetails.tsx';
+import { Container } from 'react-bootstrap';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-wrapper">
+      <Router>
+        <NavMenu />
+        <Container className="mt-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MovieListPage />} />
+            <Route path="/movies/create" element={<MovieCreatePage />} />
+            <Route path="/movies/:id" element={<MovieDetails />} />
+          </Routes>
+        </Container>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
