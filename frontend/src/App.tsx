2c5f7dff4b2f2d39fdc8bd0a48882382c7bfe5e0
morri <morrisan2001@gmail.com>
2025-02-15 13:20:51 +0100

    Add authentication routes and AuthProvider to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,13 @@ import MovieListPage from './pages/MovieListPage.tsx';
 import MovieCreatePage from './pages/MovieCreatePage.tsx';
 import MovieDetails from './pages/MovieDetails.tsx';
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from './contexts/AuthContext.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+import RegisterPage from './pages/RegisterPage.tsx';
 
 const App: React.FC = () => {
   return (
-    <div className="app-wrapper">
+    <AuthProvider>
       <Router>
         <NavMenu />
         <Container className="mt-4">
@@ -19,11 +22,13 @@ const App: React.FC = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/movies" element={<MovieListPage />} />
             <Route path="/movies/create" element={<MovieCreatePage />} />
-            <Route path="/movies/:id" element={<MovieDetails />} />
+            <Route path="/movies/details" element={<MovieDetails />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </Container>
       </Router>
-    </div>
+    </AuthProvider>
   );
 };
 
